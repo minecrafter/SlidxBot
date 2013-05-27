bfa708441161c45ec47d5d327471ef3546d23409
tuxed <tux-amd64@tabuu>
2013-05-27 13:27:12 -0400

    Fixed

---
+++ SlidxBot/ConfigurationSample.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace SlidxBot
+{
+	public static class Configuration
+	{
+		//
+		// ####### THIS IS A SAMPLE FILE! #######
+		//
+		// Rename it to Configuration.cs.
+		//
+		public static string Username = "slidx";
+		public static string Password = "disco";
+	}
+}
+
